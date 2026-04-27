9b58e26cddfd6d514294223409f04478585ea57e
lamb91 <marco.lambruschi01@gmail.com>
2026-04-27 16:41:52 +0200

    fix: ripristino vite config originale

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard-utility-operations/',
-  server: {
-    headers: {
-      'X-Frame-Options': '',
-      'Content-Security-Policy': '',
-    }
-  }
 })
